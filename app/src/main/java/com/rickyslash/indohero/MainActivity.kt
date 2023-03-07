52d250070ea3c2d39b5e7194cc3d47ce5fc16a58
ricky-kiva <ricky.kiva@gmail.com>
2023-03-07 19:52:24 +0700

    implement ListHeroAdapter

--- app/src/main/java/com/rickyslash/indohero/MainActivity.kt
+++ app/src/main/java/com/rickyslash/indohero/MainActivity.kt
@@ -2,10 +2,53 @@ package com.rickyslash.indohero
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.rickyslash.indohero.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val heroList = ArrayList<Hero>()
+
+    private fun showSelectedHero(hero: Hero) {
+        Toast.makeText(this, "You've chosen ${hero.name}", Toast.LENGTH_SHORT).show()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.rvHeroes.setHasFixedSize(true)
+        heroList.addAll(getListHeroes())
+    }
+
+    private fun getListHeroes(): ArrayList<Hero> {
+        val dataName = resources.getStringArray(R.array.data_name)
+        val dataDesc = resources.getStringArray(R.array.data_description)
+        val dataPhoto = resources.getStringArray(R.array.data_photo)
+        val listHero = ArrayList<Hero>()
+
+        for (i in dataName.indices) {
+            val hero = Hero(dataName[i], dataDesc[i], dataPhoto[i])
+            listHero.add(hero)
+        }
+
+        return listHero
+    }
+
+    private fun showRecyclerList() {
+        binding.rvHeroes.layoutManager = LinearLayoutManager(this)
+
+        val listHeroAdapter = ListHeroAdapter(heroList)
+        binding.rvHeroes.adapter = listHeroAdapter
+
+        listHeroAdapter.setOnItemClickCallback(object : ListHeroAdapter.OnItemClickCallback {
+            override fun onItemClicked(data: Hero) {
+                showSelectedHero(data)
+            }
+        })
     }
 }
