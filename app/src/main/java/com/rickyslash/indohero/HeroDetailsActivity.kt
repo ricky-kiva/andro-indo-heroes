fe9d0009b5f9e3742ad764a9dc76957479f22a46
ricky-kiva <ricky.kiva@gmail.com>
2023-03-08 12:35:47 +0700

    add share button

--- app/src/main/java/com/rickyslash/indohero/HeroDetailsActivity.kt
+++ app/src/main/java/com/rickyslash/indohero/HeroDetailsActivity.kt
@@ -1,8 +1,11 @@
 package com.rickyslash.indohero
 
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.bumptech.glide.Glide
 import com.rickyslash.indohero.databinding.ActivityHeroDetailsBinding
 
@@ -19,12 +22,7 @@ class HeroDetailsActivity : AppCompatActivity() {
         binding = ActivityHeroDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val hero = if (Build.VERSION.SDK_INT >= 33) {
-            intent.getParcelableExtra(EXTRA_HERO, Hero::class.java)
-        } else {
-            @Suppress("DEPRECATION")
-            intent.getParcelableExtra(EXTRA_HERO)
-        }
+        val hero = getHeroParcel(intent)
 
         if (hero != null) {
             binding.detailsName.text = hero.name
@@ -33,6 +31,40 @@ class HeroDetailsActivity : AppCompatActivity() {
                 .load(hero.photo)
                 .into(binding.detailsImg)
         }
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.share_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        val hero = getHeroParcel(intent)
+
+        when (item.itemId) {
+            R.id.action_share -> {
+                if (hero != null) {
+                    val shareIntent = Intent(Intent.ACTION_SEND)
+                    shareIntent.setType("text/plain")
+
+                    val descSend = "Article – ${hero.name}\n\n${hero.desc}"
+
+                    shareIntent.putExtra(Intent.EXTRA_SUBJECT, hero.name)
+                    shareIntent.putExtra(Intent.EXTRA_TEXT, descSend)
+                    startActivity(Intent.createChooser(shareIntent, "Share the Hero"))
+                }
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
+
+private fun getHeroParcel(intent: Intent): Hero? {
+    return if (Build.VERSION.SDK_INT >= 33) {
+        intent.getParcelableExtra(HeroDetailsActivity.EXTRA_HERO, Hero::class.java)
+    } else {
+        @Suppress("DEPRECATION")
+        intent.getParcelableExtra(HeroDetailsActivity.EXTRA_HERO)
     }
 }
